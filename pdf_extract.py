a905aaecc2a4c5ea761a3390ea3cbc3ccb10a593
ashwanidhankhar <45159939+ashwanidhankhar@users.noreply.github.com>
2019-04-11 15:49:06 +0530

    Add files via upload

---
+++ pdf_extract.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 11:25:04 2019
+
+@author: ashwa
+"""
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter#process_pdf
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+
+from cStringIO import StringIO
+
+def pdf_to_text(pdfname):
+
+    # PDFMiner boilerplate
+    rsrcmgr = PDFResourceManager()
+    sio = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, sio, codec=codec, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    # Extract text
+    fp = file(pdfname, 'rb')
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+    fp.close()
+
+    # Get text from StringIO
+    text = sio.getvalue()
+
+    # Cleanup
+    device.close()
+    sio.close()
+
+    return text
+
+f = "1206741.pdf"
+data =  pdf_to_text(f)
+new = open("pdf_text_doc.txt","w")
+new.write(data)
+new.close()
